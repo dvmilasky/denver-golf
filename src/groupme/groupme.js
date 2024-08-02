fd5c1535ccff4ccf0a553e141987a904cd2a09bc
David Milasky <dvmilasky@outlook.com>
2024-08-01 23:13:18 -0600

    rm logs, bug

--- src/groupme/groupme.js
+++ src/groupme/groupme.js
@@ -10,7 +10,6 @@ async function post_message(text) {
     const endpoint = groupmeConfig.post_message_endpoint;
     do {
         let chunk = text.slice(0, groupmeConfig.max_message_length);
-        console.log(`CH: ${chunk}`);
         const body = {
             "bot_id": bot_id,
             "text": chunk
@@ -41,18 +40,22 @@ async function parse_message(text) {
         post_message(msg);
     }
     else if (cmd == "saturday") {
-        const tee_times = await get_saturday_tee_times(args[1], args[2], args[3]);
+        const earliest_hour = args[1];
+        const latest_hour = args[2];
+        const num_golfers = args[3];
+        const num_holes = args[4];
+
+        const tee_times = await get_saturday_tee_times(earliest_hour, latest_hour, num_golfers, num_holes);
         if (tee_times.length > 0) {
             const tee_time = tee_times.pop();
-            const msg = await book_tee_time(tee_time.teeSheetId);
-            post_message(`***********SATURDAY TEE TIME BOOKED***************\n${build_tee_time_text(tee_time)}\n${msg}`);
+            const msg = await book_tee_time(tee_time.teeSheetId, num_golfers, num_holes);
+            post_message(`********SATURDAY TEE TIME BOOKED********\n${build_tee_time_text(tee_time)}\n${msg}`);
         } else {
             post_message("Unable to book Saturday Tee Time - Try with less golfers or adjusted time window");
         }
     }
 }
 
-await parse_message("saturday 8 13 4 18");
 
 export {
     parse_message
--- src/legacy_ridge/legacy_ridge.js
+++ src/legacy_ridge/legacy_ridge.js
@@ -99,7 +99,7 @@ async function get_saturday_tee_times(earliest_hour, latest_hour, num_golfers, h
     
     return tee_times.filter((teeTime) => {
         let startTime = new Date(teeTime.startTime).getHours();
-        return (startTime <= latest_hour && startTime >= earliest_hour && teeTime.holes == holes && teeTime.participants == num_golfers);
+        return (startTime <= latest_hour && startTime >= earliest_hour && num_golfers == teeTime.participants && holes == teeTime.holes);
     });
 }
 
@@ -114,7 +114,6 @@ async function get_tee_times(date, num_golfers=0, holes=0) {
         if (error.response) {
             console.error(error.response.data);
         }
-        return [];
     });
     if (Array.isArray(res.data)) {
         return res.data;
@@ -147,10 +146,12 @@ async function book_tee_time(tee_sheet_id, num_golfers, holes) {
     const res = await axios.post(endpoint_url, payload, {
         headers: build_book_tee_times_headers(api_key, website_id, login_token),
     }).catch(function (error) {
-        console.log("Error: " + error.message);
-        return "Error Booking - Check Logs"
+        console.error("Error: " + error.message);
     });
-    return `Confirmation ID: ${res.data.reservationId}`;
+    if (res.data) {
+        return `Confirmation ID: ${res.data.reservationId}`;
+    }
+    return `Error Booking Tee Sheet ID: ${tee_sheet_id}`
 }
 
 export {
